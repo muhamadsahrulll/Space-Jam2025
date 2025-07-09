5e534a00e627567b99bce32c70aefd13fba73ef5
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2025-07-09 11:14:46 +0700

    closed testing

--- Assets/Video/splashscreen.cs
+++ Assets/Video/splashscreen.cs
@@ -6,17 +6,42 @@ using UnityEngine.Video;
 
 public class splashscreen : MonoBehaviour
 {
-    public VideoPlayer videoPlayer;  // drag VideoPlayer ke sini di Inspector
+    public VideoPlayer videoPlayer;         // Drag VideoPlayer ke sini di Inspector
+    public VideoClip[] videoClips;          // Isi array VideoClip di Inspector
     public string nextSceneName = "Main Menu";
+
+    private int currentVideoIndex = 0;
 
     void Start()
     {
-        // Subscribe event ketika video selesai
-        videoPlayer.loopPointReached += OnVideoFinished;
+        if (videoClips.Length > 0)
+        {
+            videoPlayer.loopPointReached += OnVideoFinished;
+            PlayCurrentVideo();
+        }
+        else
+        {
+            // Kalau tidak ada video, langsung ke scene berikutnya
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+
+    void PlayCurrentVideo()
+    {
+        videoPlayer.clip = videoClips[currentVideoIndex];
+        videoPlayer.Play();
     }
 
     void OnVideoFinished(VideoPlayer vp)
     {
-        SceneManager.LoadScene(nextSceneName);
+        currentVideoIndex++;
+        if (currentVideoIndex < videoClips.Length)
+        {
+            PlayCurrentVideo();
+        }
+        else
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
     }
 }
